b3c19375f11cf2588f53e8b9c2ec9665c26da4eb
ludolunapa <ll94_@hotmail.com>
2016-11-10 07:06:23 -0700

    Add files via upload

--- Restaurante/datos_prueba/generador.py
+++ Restaurante/datos_prueba/generador.py
@@ -8,20 +8,18 @@ from random import choice
 import io
 from random import randint
 def generacion():
-        file = open("Reservaciones","w")
-        
-        for i in range(20):
-                nombre=choice(nom)+" "+choice(ape)+" "+choice(ape)
-                tel="311"+str(randint(100,999))+choice(nums)+choice(nums)+choice(nums)+choice(nums)
-                hora=str(randint(0,12))+":"+str(randint(0,5))+choice(nums)
-                fecha=str(randint(1,12))+"/"+str(randint(1,30))+"/"+"2016"
-		status=choice(stat)
-		usuario=choice(usr)
-                # id | nombre | tel | hora | fecha | status | usuario
-                file.write(str(i)+"|"+nombre+"|"+tel+"|"+hora+"|"+fecha+"|"+status+"|"+usuario+"\n")
-                #file.write(i+"|"+nombre+"|"+tel+"|"+hora+"|"+fecha+"\n")
-
-        file.close()
+    file = open("Reservaciones","w")
+    for i in range(20):
+        nombre=choice(nom)+" "+choice(ape)+" "+choice(ape)
+        tel="311"+str(randint(100,999))+choice(nums)+choice(nums)+choice(nums)+choice(nums)
+        hora=str(randint(0,12))+":"+str(randint(0,5))+choice(nums)
+        fecha=str(randint(1,12))+"/"+str(randint(1,30))+"/"+"2016"
+        status=choice(stat)
+        usuario=choice(usr)
+        # id | nombre | tel | hora | fecha | status | usuario
+        file.write(str(i)+"|"+nombre+"|"+tel+"|"+hora+"|"+fecha+"|"+status+"|"+usuario+"\n")
+        #file.write(i+"|"+nombre+"|"+tel+"|"+hora+"|"+fecha+"\n")
+    file.close()
 
 	
 generacion()
@@ -31,4 +29,3 @@ file= open("Reservaciones","r")
 
 print(file.read())
 file.close()
-
