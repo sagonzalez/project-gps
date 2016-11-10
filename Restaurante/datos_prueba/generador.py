a4df0a9d32904598794316d73a47c9ca8e9e21e4
ludolunapa <ll94_@hotmail.com>
2016-11-10 06:23:50 -0700

    Update generador.py

--- Restaurante/datos_prueba/generador.py
+++ Restaurante/datos_prueba/generador.py
@@ -2,6 +2,8 @@ ape=["Salcedo","López","Órtiz","Castaña","Iturbide","Gárces","Moreno","Ibarr
 
 nom=["Luis","Juan","Salvador","Ivan","Alan","Daniel","Carlos","Pablo","Alejandro","Maria","Giovani","Jovani","Cesar","Oliver","Esteban","Efraín","Kevin","Brayan","Bryan","Luisa","Carla","Dolores","Perla","Estefania","Xochitl","Mariela","Hilda","Gracia","Federico","Ines","Ana","Ariana"]
 nums=["0","1","2","3","4","5","6","7","8","9"]
+stat=["Pendiente","Aceptado","Cancelado"]
+usr=["cadicarus","pepecort","lilabel","cascaso","elmocos"]
 from random import choice
 import io
 from random import randint
@@ -13,8 +15,10 @@ def generacion():
                 tel="311"+str(randint(100,999))+choice(nums)+choice(nums)+choice(nums)+choice(nums)
                 hora=str(randint(0,12))+":"+str(randint(0,5))+choice(nums)
                 fecha=str(randint(1,12))+"/"+str(randint(1,30))+"/"+"2016"
-                # id | nombre | tel | hora | fecha | 
-                file.write(str(i)+"|"+nombre+"|"+tel+"|"+hora+"|"+fecha+"\n")
+		status=choice(stat)
+		usuario=choice(usr)
+                # id | nombre | tel | hora | fecha | status | usuario
+                file.write(str(i)+"|"+nombre+"|"+tel+"|"+hora+"|"+fecha+"|"+status+"|"+usuario+"\n")
                 #file.write(i+"|"+nombre+"|"+tel+"|"+hora+"|"+fecha+"\n")
 
         file.close()
